9da92d60497754c767601ca1dc18695efdb22ef2
ShinaSaiyad <shinasaiyad22@gmail.com>
2025-01-08 11:58:11 +0530

    update heading

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD</title>
+    <title>CRUD-operation</title>
 </head>
 <body>
     
